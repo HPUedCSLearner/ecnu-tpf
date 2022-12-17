83d26ca0b837f3858f0ceaa46ecf4b1a0ca5a430
maple <maple_e123@163.com>
2022-12-17 05:04:32 -0800

    bugfix cpp probe lib

--- timerSharedStaticCorCXX/version0.2.0-cpp/demo1/probelib/sharedlib/probeImpl.c
+++ timerSharedStaticCorCXX/version0.2.0-cpp/demo1/probelib/sharedlib/probeImpl.c
@@ -4,13 +4,13 @@
 #include "hook.h"
 
 void __self__traceBegin() {
-    printf("__self__traceBegin\n");
+    __traceBegin_hook();
 }
 void __self__traceEnd() {
-    printf("__self__traceEnd\n");
+    __traceEnd_hook();
 }
 void __self__cyg_profile_func_enter(void *func, void *caller) {
-     __entr_cpp_hook(func, caller);
+    __entr_cpp_hook(func, caller);
 }
 void __self__cyg_profile_func_exit(void *func, void *caller) {
     __exit_cpp_hook(func, caller);
