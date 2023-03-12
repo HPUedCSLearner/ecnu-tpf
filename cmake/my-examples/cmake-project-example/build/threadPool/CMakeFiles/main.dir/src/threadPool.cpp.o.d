713ef1a6d15df283849736ef4c03decdc6c551b2
root <docker1.com>
2023-03-12 01:39:19 +0000

    remove build things

--- cmake/my-examples/cmake-project-example/build/threadPool/CMakeFiles/main.dir/src/threadPool.cpp.o.d
+++
@@ -1,3 +0,0 @@
-threadPool/CMakeFiles/main.dir/src/threadPool.cpp.o: \
- /home/tmpDevDir/ecnu-tpf/cmake/my-examples/cmake-project-example/threadPool/src/threadPool.cpp \
- /usr/include/stdc-predef.h
