9e5f0304f0a0226c8a12c428273f4e8cfec86460
wangfeng <yoga13s-wsl2-ubuntu22.04>
2023-11-07 22:03:03 +0800

    add config env

---
+++ configEnv/configEnv.sh
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+#build-essential
+sudo apt install ninja-build  build-essential net-tools htop
+
+
+
+#git
+sudo apt install git
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.st status
+
+#git user name
+git config --global user.email "yoga13s-wsl2-ubuntu22.04"
+git config --global user.name "wangfeng"
+
+
+
---
+++ configEnv/pipInstall.sh
@@ -0,0 +1,1 @@
+pip install scipy numpy torch torchvision  matplotlib h5py  -i https://pypi.tuna.tsinghua.edu.cn/simple
