f5aed5cd3ccec8b5d59b69b32c77d17d506312c7
feng-dell-linux <maple_e123@163.com>
2023-11-09 21:25:42 +0800

    add some set env file

---
+++ setEnv/set-llvm-env.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/usr/local/llvm-15.0.6/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/llvm-15.0.6/lib
+
---
+++ setEnv/set-openblas-env.sh
@@ -0,0 +1,6 @@
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/home/feng/wys/self-install/include/openblas/
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/home/feng/wys/self-install/include/openblas/
+
+export LIBRARY_PATH=$LIBRARY_PATH:/home/feng/wys/self-install/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/feng/wys/self-install/lib/
+
